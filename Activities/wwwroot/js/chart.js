9296897e36936fdf477348e6c6cdd8e1ae7dd668
victor bravo <ahal_tocob@hotmail.com>
2022-04-19 12:57:36 -0500

    Chat rc

---
+++ Activities/wwwroot/js/chart.js
@@ -0,0 +1,89 @@
+﻿var url;
+
+url = "/Home/ActivitySummary";
+fetch(url)
+    .then(response => {
+        if (response.ok) {
+            return response.json();
+        }
+    })
+    .then(response => {
+        //console.log(response);
+        response.forEach(element => {
+            var labels;
+            var data;
+            var colors;
+
+            labels = getLabels(element);
+            data = getData(element);
+            colors = getColors(element);
+
+            buildChart("chart_" + element.id, labels, data, colors);
+        });
+    });
+
+function getLabels(activitySummary) {
+    var labels;
+
+    labels = new Array();
+    activitySummary.listRowStatus.forEach(element => {
+        labels.push(element.name);
+    });
+
+    return labels;
+}
+
+function getData(activitySummary) {
+    var data;
+
+    data = new Array();
+    activitySummary.listRowStatus.forEach(element => {
+        data.push(element.total);
+    });
+
+    return data;
+}
+
+function getColors(activitySummary) {
+    var data;
+
+    data = new Array();
+    activitySummary.listRowStatus.forEach(element => {
+        data.push(element.color);
+    });
+
+    return data;
+}
+
+function buildChart(chartId, labels, data, colors) {
+    var ctx = document.getElementById(chartId);
+    var myPieChart = new Chart(ctx, {
+        type: 'doughnut',
+        data: {
+            labels: labels,
+            datasets: [{
+                data: data,
+                backgroundColor: colors,
+                hoverBackgroundColor: colors,
+                hoverBorderColor: "rgba(234, 236, 244, 1)",
+            }],
+        },
+        options: {
+            maintainAspectRatio: false,
+            tooltips: {
+                backgroundColor: "rgb(255,255,255)",
+                bodyFontColor: "#858796",
+                borderColor: '#dddfeb',
+                borderWidth: 1,
+                xPadding: 15,
+                yPadding: 15,
+                displayColors: false,
+                caretPadding: 10,
+            },
+            legend: {
+                display: false
+            },
+            cutoutPercentage: 80,
+        },
+    });
+}
---
+++ Activities/wwwroot/js/chat.js
@@ -0,0 +1,188 @@
+﻿let conversationInterval;
+var lastChatId = 1;
+var isBussy = false;
+
+$('document').ready(function () {
+    getListUsers();
+});
+
+function repeatEveryXSeconds() {
+    console.log("inicio")
+    conversationInterval = setInterval(isLastChatId, 2000);
+}
+
+function getListUsers() {
+    var url;
+
+    url = "/api/users";
+    fetch(url).then(
+        response => {
+            if (response.ok) {
+                return response.json();
+            } else {
+                throw response;
+            }
+        }
+    ).then(response => {
+        //console.log(response);
+        fillListUsers(response);
+    });
+}
+
+function fillListUsers(response) {
+    let divList;    
+
+    divList = document.getElementById("divListUusers");
+    response.forEach(element => {
+        var div;
+        var divStatus;
+        var divRow;
+
+        divRow = document.createElement("div");
+        div = document.createElement("div");
+        divStatus = document.createElement("div");
+        divRow.className = "row"
+        div.innerHTML = element.name;
+        div.className = "col-md-7"
+        divStatus.className = "col-md-5"
+        divStatus.innerHTML = "<p class='text-success' id='user_" + element.id + "'> <button class='btn btn-success btn-sm' onclick='onclick_chatear(" + userIdSource + "," + element.id + "," + '"' + element.name + '"' + ");'>Chatear</button> </p>"
+        divRow.appendChild(div)
+        divRow.appendChild(divStatus)
+        divList.append(divRow);
+        //console.log(element)
+    });
+}
+
+function onclick_chatear(userIdSource, userIdDestiny, userNameDestiny) {
+    $("#profile-tab").tab('show');
+    document.getElementById('userIdSource').value = userIdSource;
+    document.getElementById('userIdDestiny').value = userIdDestiny;
+    document.getElementById('userNameDestiny').innerHTML = userNameDestiny;
+    document.getElementById('message').focus();
+    getConversation();
+    repeatEveryXSeconds();
+}
+
+function onclick_sendMessage() {
+    var myHeaders = new Headers();
+    myHeaders.append("accept", "*/*");
+    myHeaders.append("Content-Type", "application/json");
+
+    var raw = JSON.stringify({
+        "userIdSource": document.getElementById("userIdSource").value,
+        "message": document.getElementById("message").value,
+        "userIdDestiny": document.getElementById("userIdDestiny").value
+    });
+
+    var requestOptions = {
+        method: 'POST',
+        headers: myHeaders,
+        body: raw,
+        redirect: 'follow'
+    };
+
+    fetch("/Api/Chat", requestOptions)
+        .then(response => {
+            if (response.ok)
+                return response.json();
+            else
+                throw response;
+        })
+        .then(result => {
+            //console.log(result);
+            document.getElementById("message").value = '';
+            getConversation();
+        })
+        .catch((error) => { console.log(error) });
+}
+
+function getConversation() {
+    var userIdSource;
+    var userIdDestiny;
+    var url;
+
+    userIdSource = document.getElementById("userIdSource").value
+    userIdDestiny = document.getElementById("userIdDestiny").value
+    url = "/Api/Chat/" + userIdSource + "/" + userIdDestiny
+    if (isBussy == false) {
+        isBussy = true
+        fetch(url)
+            .then(response => {
+                if (response.ok)
+                    return response.json();
+                else
+                    throw response;
+            })
+            .then(result => {
+                isBussy = true
+                //console.log(result);
+                var conversation
+
+                conversation = document.getElementById("divConversation");
+                conversation.innerHTML = '';
+                result.forEach(item => {
+                    var p
+                    var blockquote;
+
+                    p = document.createElement("p");
+                    if (item.userIdSource == userIdSource)
+                        p.className = "text-left"
+                    else
+                        p.className = "text-right"
+                    p.innerHTML = item.message + "<footer class='blockquote-footer'>" + item.userNameSource + "</footer>";
+                    blockquote = document.createElement("blockquote")
+                    //blockquote.className = "blockquote"
+                    blockquote.append(p)
+                    conversation.append(blockquote)
+                    setLastChatId(item.id)
+                });
+                isBussy = false
+            })
+            .catch(error => { console.log(error); isBussy = false; });
+    }
+}
+
+function setLastChatId(lastId) {
+    if (lastChatId <= lastId)
+        lastChatId = lastId
+}
+
+function isLastChatId() {
+    //debugger;
+    var url
+
+    url = "/Api/Chat/" + lastChatId + "/IsLast"
+    if (isBussy == false) {
+        isBussy = true
+        fetch(url)
+            .then(response => {
+                if (response.ok)
+                    return response.json();
+                else
+                    throw response;
+            })
+            .then(result => {
+                //debugger;
+                console.log(result);
+                if (result.isLast) {
+                    //tenemos el ultimo chatId
+                } else {
+                    console.log("Obteniendo conversación")
+                    getConversation();
+                }
+                isBussy = false
+            })
+            .catch(error => {
+                isBussy = false
+                console.log(error)
+            });
+    }
+}
+
+function onkeyup_detectEnter(event) {
+    var keycode = event.keyCode;
+    //console.log(keycode)
+    if (keycode == '13') {
+        onclick_sendMessage();
+    }
+}
